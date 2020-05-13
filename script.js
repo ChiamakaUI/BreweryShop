efdd65e0e678bed458209ad71aa6a37c1219f452
Chiamaka <ezembachiamaka@gmail.com>
2020-05-13 05:53:19 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const sub = document.getElementById("ada");
+const msg = document.querySelector('.msg');
+
+sub.addEventListener("click", function(event){
+    alert("We have received your message, we will get back to you shortly")
+   //msg.innerHTML = "We have received your message and we will get back to you shortly";
+});
